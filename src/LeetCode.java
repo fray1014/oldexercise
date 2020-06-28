da0f75fc19a0dfb109275e68b5c64aa33c919d17
Fray <fray1014@163.com>
2020-06-28 17:04:57 +0800

    剪绳子

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -214,6 +214,51 @@ public class LeetCode {
             return Math.max(leftDeepth,rightDeepth)+1;
         }
     }
+
+    /**
+     * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。*/
+    public static class Solution4{
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+
+        public void push(int node) {
+            stack1.push(node);
+        }
+
+        public int pop() {
+            if(stack2.empty()){
+                while(!stack1.empty()){
+                    stack2.push(stack1.peek());
+                    stack1.pop();
+                }
+            }
+            int ret = stack2.peek();
+            stack2.pop();
+            return ret;
+        }
+    }
+
+    /**
+     * 给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1，m<=n），
+     * 每段绳子的长度记为k[1],...,k[m]。请问k[1]x...xk[m]可能的最大乘积是多少？
+     * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。*/
+    public static class Solution5{
+        public int cutRope(int target) {
+           if(target<=0) return 0;
+                if(target==1 || target == 2) return 1;
+                if(target==3) return 2;
+                int m = target % 3;
+                switch(m){
+                    case 0 :
+                        return (int) Math.pow(3, target / 3);
+                    case 1 :
+                        return (int) Math.pow(3, target / 3 - 1) * 4;
+                    case 2 :
+                        return (int) Math.pow(3, target / 3) * 2;
+                }
+                return 0;
+        }
+    }
 }
 
 
